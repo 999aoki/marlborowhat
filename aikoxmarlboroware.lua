7f7c0f041cf20353613683195f721dd243ec7029
Aoki Neil <aokineil25@gmail.com>
2025-12-17 16:22:02 +0800

    Update aikoxmarlboroware.lua

--- aikoxmarlboroware.lua
+++ aikoxmarlboroware.lua
@@ -1,6 +1,6 @@
 local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/gamelist.lua"))()
 
-local URL = Games[game.GameId]
+local URL = gs[game.GameId]
 
 if URL then
   loadstring(game:HttpGet(URL))()
