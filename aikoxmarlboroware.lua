4305bb148bea6ab31353df3ceb1e54a1e2a190b9
Aoki Neil <aokineil25@gmail.com>
2025-12-17 16:15:18 +0800

    Update aikoxmarlboroware.lua

--- aikoxmarlboroware.lua
+++ aikoxmarlboroware.lua
@@ -1,9 +1,7 @@
 local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/gamelist.lua"))()
 
-for GameID, Execute in pairs(gs) do
-    if GameID == game.GameId then
-        print("Match found! Loading script for", game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
-        loadstring(game:HttpGet(Execute))()
-        break
-    end
+local URL = Games[game.GameId]
+
+if URL then
+  loadstring(game:HttpGet(URL))()
 end
