975c6b58a6f8eea9368f1518902c561e396525ff
Aoki Neil <aokineil25@gmail.com>
2025-12-17 16:37:47 +0800

    Update aikoxmarlboroware.lua

--- aikoxmarlboroware.lua
+++ aikoxmarlboroware.lua
@@ -1,5 +1,12 @@
 local gs = loadstring(game:HttpGet("https://raw.githubusercontent.com/a11bove/kdoaz/refs/heads/main/loader/gamelist.lua"))()
-local URL = gs[game.GameId] or gs[game.PlaceId]
+
+local URL = gs[game.PlaceId] or gs[game.GameId]
+
 if URL then
+    print("Loading script for:", game.PlaceId, "or", game.GameId)
     loadstring(game:HttpGet(URL))()
+else
+    warn("No script found for this game.")
+    warn("PlaceId: " .. tostring(game.PlaceId))
+    warn("GameId: " .. tostring(game.GameId))
 end
